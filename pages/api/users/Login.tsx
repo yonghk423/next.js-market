13cc94d2924ba9b1fce59846a85f228d42acabda
yonghk423 <yonghk423423@gmail.com>
2022-05-31 12:47:17 +0900

    token set up

--- pages/api/users/Login.tsx
+++ pages/api/users/Login.tsx
@@ -19,48 +19,65 @@ interface Data {
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  const { phone, email }:Data = req.body;
-  console.log(phone);
-  let user;
-  if(email) {
-      user = await client.user.findUnique({
-      where: {
-        email: email
+  const { phone, email }:Data = req.body;  
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
       },
-    });
-    if(user) console.log("found it.")
-    if(!user) {
-      console.log("Did not find. Will create")
-      user = await client.user.create({
-        data: {
-          name:"Anonymous",
-          email:email,
-        }
-      })
-    }
-    console.log(user);
-  }
-//--------------------------------------------------------------------------------
-  if(phone) {
-    // console.log(phone);
-      user = await client.user.findUnique({
-      where: {
-        phone:+phone,
-      },
-    });
-    if(user) console.log("found it.")
-    if(!user) {
-      console.log("Did not find. Will create")
-      user = await client.user.create({
-        data: {
-          name:"Anonymous",
-          phone:+phone,
-        }
-      })
-    }
-    console.log(user);
-  }
+    },    
+  });
+  console.log(token)
   return res.status(200).end()
+  // if(email) {
+  //     user = await client.user.findUnique({
+  //     where: {
+  //       email: email
+  //     },
+  //   });
+  //   if(user) console.log("found it.")
+  //   if(!user) {
+  //     console.log("Did not find. Will create")
+  //     user = await client.user.create({
+  //       data: {
+  //         name:"Anonymous",
+  //         email:email,
+  //       }
+  //     })
+  //   }
+  //   console.log(user);
+  // }
+//--------------------------------------------------------------------------------
+  // if(phone) {
+  //   // console.log(phone);
+  //     user = await client.user.findUnique({
+  //     where: {
+  //       phone:+phone,
+  //     },
+  //   });
+  //   if(user) console.log("found it.")
+  //   if(!user) {
+  //     console.log("Did not find. Will create")
+  //     user = await client.user.create({
+  //       data: {
+  //         name:"Anonymous",
+  //         phone:+phone,
+  //       }
+  //     })
+  //   }
+  //   console.log(user);
+  // }
 }
 
 export default Handler("POST", handler)
