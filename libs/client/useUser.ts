39b38ee45b99326a50e58475b5fd8a949e1e92ff
yonghk423 <yonghk423423@gmail.com>
2022-06-03 00:35:21 +0900

    swr set up

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,18 +1,21 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
+import useSWR from 'swr';
 
+const fetcher = (url:string) => fetch(url).then((response) => response.json());
 export default function useUser() {
-    const [user, setUser] = useState();
+    const {data, error} = useSWR("api/users/Me", fetcher)
+    // const [user, setUser] = useState();
     const router = useRouter();
-    useEffect(() => {
-        fetch("api/users/Me")
-        .then((response) => response.json())
-        .then((data) => {
-            if(!data.ok) {
-                return router.replace("/Login")
-            }
-            setUser(data.profile);
-        })
-    }, [router])
-    return user;
+    // useEffect(() => {
+    //     fetch("api/users/Me")
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         if(!data.ok) {
+    //             return router.replace("/Login")
+    //         }
+    //         setUser(data.profile);
+    //     })
+    // }, [router])
+    return data;
 }
