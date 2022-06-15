44be2791dff27ec7ecac2f6b81e0855145226101
yonghk423 <yonghk423423@gmail.com>
2022-06-15 17:12:53 +0900

    loved page

--- pages/privacy/Loved.tsx
+++ pages/privacy/Loved.tsx
@@ -4,6 +4,35 @@ import useSWR from 'swr'
 import { Product } from '@prisma/client'
 import { useRouter } from "next/router";
 
+const Container = styled.div`  
+  padding: 10px;
+`;
+const ItemListBox = styled.div`
+  border: 1px solid black;
+  display: flex;
+  border: 1px solid gray;
+  padding: 4px;  
+  justify-content: space-between;
+`;
+const ImgBox = styled.img`
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: gray;
+  cursor: pointer;
+`
+const ItemBox = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+`;
+const InfoBox = styled.div`
+`;
+const StateImgBox = styled.div``;
+
+const BtnBox = styled.div`
+`;
+
 const Svg = styled.svg`
 width: 25px;
 height: 25px;
@@ -22,26 +51,37 @@ interface ProductsResponse {
 
 
 const Loved: NextPage = () => {
-  const { data } = useSWR<ProductsResponse>("/api/users/me/favs")
+  const router = useRouter()
+  const { data } = useSWR("/api/users/me/favs")
   console.log(data);
 
+  const onProductClick = (id:number) => {
+    router.push(
+      {
+        pathname: `/products/${id}`,
+        query: {
+          id
+        },
+      },
+      
+    );
+  }
+
   return (
-    <div>
-      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-        >
-          <div>
-            <div/>
-            <div>
-              <h3>
-                New iPhone 14
-              </h3>
-              <span>Black</span>
-              <span>$95</span>
-            </div>
-          </div>
-          <div>
+     <Container>
+       {data?.favs?.map((fav:any) => (
+        <ItemListBox key={fav.id}>          
+          <ItemBox onClick={() => onProductClick(fav?.product?.id)}>  
+            <ImgBox
+             src={`https://imagedelivery.net/KIkx1DioUEY-Y5COTODk1Q/${fav?.product?.image}/public`} //Variants 다시 공부           
+             alt=""
+            />
+            <InfoBox>
+              <h3>{fav?.product?.name}</h3> 
+              <div>₩{fav?.product?.price}</div>                      
+            </InfoBox>            
+          </ItemBox>
+          <StateImgBox>
             <div>
               <Svg
                 className="w-4 h-4"
@@ -57,7 +97,7 @@ const Loved: NextPage = () => {
                   d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                 ></path>
               </Svg>
-              <span>1</span>
+              <div>{fav?.product._count?.favs}</div>
             </div>
             <div>
               <Svg
@@ -74,13 +114,13 @@ const Loved: NextPage = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 ></path>
               </Svg>
-              <span>1</span>
+              <div>1</div>
             </div>
-          </div>
-        </div>
+          </StateImgBox>
+        </ItemListBox>
       ))}
-    </div>
-  );
+    </Container>    
+  )
 };
 
 export default Loved;
