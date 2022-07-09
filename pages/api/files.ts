35aaac3de0df163c24a772c4fa18fe5a6ca4a93c
yonghk423 <yonghk423423@gmail.com>
2022-07-09 21:58:32 +0900

    test

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -3,11 +3,11 @@ import client from '../../libs/server/client';
 import Handler, { ResponseType } from "../../libs/server/Handler"
 import { withIronSessionApiRoute } from "iron-session/next";
 import { withApiSession } from '../../libs/server/WithSession';
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  console.log(req.body);
   const response = await (
     await fetch(
       `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ID}/images/v1/direct_upload`,
