1dbb5e3cc0fb9179951754d5046ab9fc17d531af
yonghk423 <yonghk423423@gmail.com>
2022-07-09 22:24:39 +0900

    test

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '../../libs/server/client';
 import Handler, { ResponseType } from "../../libs/server/Handler"
-import { withIronSessionApiRoute } from "iron-session/next";
 import { withApiSession } from '../../libs/server/WithSession';
 async function handler(
   req: NextApiRequest,
